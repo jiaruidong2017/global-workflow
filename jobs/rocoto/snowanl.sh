7cc82327412d80e9f21d883f17c926710f53c47d
Jiarui Dong <Jiarui.Dong@noaa.gov>
2024-03-18 12:57:09 -0400

    Add the snow DA analysis to update the enkf members.

--- jobs/rocoto/snowanl.sh
+++ jobs/rocoto/snowanl.sh
@@ -3,8 +3,14 @@
 source "${HOMEgfs}/ush/preamble.sh"
 
 ###############################################################
-# Source UFSDA workflow modules
-. "${HOMEgfs}/ush/load_ufsda_modules.sh"
+# Source FV3GFS workflow modules
+# TODO clean this up once ncdiag/1.1.2 is installed on WCOSS2
+source "${HOMEgfs}/ush/detect_machine.sh"
+if [[ "${MACHINE_ID}" == "wcoss2" ]]; then
+   . "${HOMEgfs}/ush/load_ufswm_modules.sh"
+else
+   . "${HOMEgfs}/ush/load_fv3gfs_modules.sh"
+fi
 status=$?
 [[ ${status} -ne 0 ]] && exit "${status}"
 
@@ -21,4 +27,5 @@ export PYTHONPATH
 # Execute the JJOB
 "${HOMEgfs}/jobs/JGLOBAL_SNOW_ANALYSIS"
 status=$?
+
 exit "${status}"
